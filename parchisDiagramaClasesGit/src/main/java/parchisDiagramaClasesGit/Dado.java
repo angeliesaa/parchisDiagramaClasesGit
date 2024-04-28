f1f9d0da2c6fecd094f810d132ce0470f26de137
angeliesaa <acrespog01@educantabria.es>
2024-04-28 20:03:44 +0200

    Inicio - Proyecto de partida: parchisClasesPruebasUnits

---
+++ parchisDiagramaClasesGit/src/main/java/parchisDiagramaClasesGit/Dado.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package parchisDiagramaClasesGit;
+
+import java.util.Random;
+
+/**
+ * 
+ */
+public class Dado {
+
+	private int numero;
+	
+	public void tirar() {
+		
+		Random aleatorio = new Random();
+		
+		numero = aleatorio.nextInt(6)+1;
+		
+	}
+
+	public int mostrarTirada() {
+	
+		return numero;
+	}
+	
+}
